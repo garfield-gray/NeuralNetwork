9b449bfaf222f72771b51025ac55098403fb8ba8
garfield-gray <garfield.gray.999@gmail.com>
2022-02-01 12:55:19 +0000

    test of copilot

---
+++ Hidden layer/test.py
@@ -0,0 +1,11 @@
+#how to read a few numbers from user
+a[3] = int(input("Enter a number: "))
+print(a)
+
+
+import numpy as np
+m = np.array([[1.1,2,3],[4,5,6],[7,8,9]])
+print(m)
+print(m.shape)
+print(m.dtype)
+
